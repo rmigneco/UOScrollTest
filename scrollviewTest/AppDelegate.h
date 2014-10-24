0b2e70e9616a62b96e502f587935e1a79cdbf030
Ray Migneco <ray.migneco@gmail.com>
2014-10-24 14:00:49 -0400

    Initial Commit

---
+++ scrollviewTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  scrollviewTest
+//
+//  Created by Ray Migneco on 10/24/14.
+//  Copyright (c) 2014 Urban Outfitters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ scrollviewTestTests/scrollviewTestTests.m
@@ -0,0 +1,34 @@
+//
+//  scrollviewTestTests.m
+//  scrollviewTestTests
+//
+//  Created by Ray Migneco on 10/24/14.
+//  Copyright (c) 2014 Urban Outfitters. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface scrollviewTestTests : XCTestCase
+
+@end
+
+@implementation scrollviewTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
